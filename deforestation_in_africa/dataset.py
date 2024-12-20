a1cb6f44a3aaad8bec5377ea1a15c3ae929ba70f
Chike Ode <chikeodenigbo@gmail.com>
2024-12-19 22:25:44 -0500

    created working code to get bounding box image

--- deforestation_in_africa/dataset.py
+++ deforestation_in_africa/dataset.py
@@ -1,4 +1,6 @@
 # https://code.earthengine.google.com/register
+# https://www.youtube.com/watch?v=_6JOA-iiEGU
+# https://github.com/google/earthengine-community/blob/master/tutorials/beginners-cookbook/index.md
 from pathlib import Path
 import os
 import numpy as np
@@ -33,16 +35,18 @@ app = typer.Typer()
 def main(
     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
     # input_path: Path = RAW_DATA_DIR  / "test.nc",
-    input_path: Path = RAW_DATA_DIR  / "test.csv",
+    input_path: Path = RAW_DATA_DIR ,# / "test.csv",
     output_path: Path = PROCESSED_DATA_DIR, # / "dataset.csv",
     # ----------------------------------------------
 ):
     # ---- REPLACE THIS WITH YOUR OWN CODE ----
     logger.info("Processing dataset...")
     start_date = '2005-05-01'
-    end_date = '2005-05-06'
+    end_date = '2005-05-04'
     # random region 
-    roi = ee.Geometry.Rectangle([-104.05, 49.05, -96.55, 45.05])
+    roi = ee.Geometry.Rectangle([4.3, 3.8, 13.9, 14.7])
+    # roi = ee.Geometry.Rectangle([-26.5, 43.2, -11.5, 50.5])
+    # roi = ee.Geometry.Rectangle([-104.05, 49.05, -96.55, 45.05])
 
     # Filter the Landsat collection
     landsat = ee.ImageCollection("LANDSAT/LE07/C02/T1_L2").filterDate(start_date, end_date).filterBounds(roi)
@@ -51,14 +55,14 @@ def main(
     # 10 -  number of images found
 
     # Create the folder if it doesn't exist
-    output_folder = 'Landsat_Tiles'
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-    out_dir = os.path.join(os.getcwd(), output_folder)
+    # input_path = 'Landsat_Tiles'
+    # if not os.path.exists(input_path):
+    #     os.makedirs(input_path)
+    # out_dir = os.path.join(os.getcwd(), input_path)
 
 
     # download images
-    geemap.download_ee_image_collection(landsat, out_dir, scale=30)
+    geemap.download_ee_image_collection(landsat, input_path, scale=30)
 
     logger.success("Processing dataset complete.")
     # -----------------------------------------
